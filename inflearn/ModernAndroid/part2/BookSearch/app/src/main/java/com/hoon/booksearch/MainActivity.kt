401acbc83078bc322dbc999e1e73e39f349b08f8
boris920308 <jhoonj0308@gmail.com>
2023-04-23 18:09:03 +0900

    [ feat ][ModernAndroid] 1.ViewBinding 적용

--- inflearn/ModernAndroid/part2/BookSearch/app/src/main/java/com/hoon/booksearch/MainActivity.kt
+++ inflearn/ModernAndroid/part2/BookSearch/app/src/main/java/com/hoon/booksearch/MainActivity.kt
@@ -2,10 +2,18 @@ package com.hoon.booksearch
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.hoon.booksearch.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
+
+
 }
