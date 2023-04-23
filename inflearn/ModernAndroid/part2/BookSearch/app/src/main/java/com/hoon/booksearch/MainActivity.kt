6e7bed97ef58c7571a05f647c9a8975089fd1813
boris920308 <jhoonj0308@gmail.com>
2023-04-23 18:27:02 +0900

    [ feat ][ModernAndroid] 1.bottom navigation 적용

--- inflearn/ModernAndroid/part2/BookSearch/app/src/main/java/com/hoon/booksearch/MainActivity.kt
+++ inflearn/ModernAndroid/part2/BookSearch/app/src/main/java/com/hoon/booksearch/MainActivity.kt
@@ -3,6 +3,9 @@ package com.hoon.booksearch
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.hoon.booksearch.databinding.ActivityMainBinding
+import com.hoon.booksearch.ui.view.FavoriteFragment
+import com.hoon.booksearch.ui.view.SearchFragment
+import com.hoon.booksearch.ui.view.SettingFragment
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,7 +16,39 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        setupBottomNavigationView()
+
+        if (savedInstanceState == null) {
+            // app 첫실행 판단, 첫 실행일 경우 SearchFragment 표시
+            binding.bottomNavigationView.selectedItemId = R.id.fragment_search
+        }
     }
 
+    private fun setupBottomNavigationView() {
+        binding.bottomNavigationView.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.fragment_search -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.frame_layout, SearchFragment())
+                        .commit()
+                    true
+                }
+                R.id.fragment_favorite -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.frame_layout, FavoriteFragment())
+                        .commit()
+                    true
+                }
+                R.id.fragment_setting -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.frame_layout, SettingFragment())
+                        .commit()
+                    true
+                }
+                else -> false
+            }
+        }
+    }
 
 }
