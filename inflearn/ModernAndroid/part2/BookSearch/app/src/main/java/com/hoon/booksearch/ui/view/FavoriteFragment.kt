7804d9191745ef2ff718eef3f70f74c9c52a91e1
boris920308 <jhoonj0308@gmail.com>
2023-04-30 21:31:00 +0900

    [ feat ][ModernAndroid] 1. Livedata -> flow 변경 (observer -> lauch)

--- inflearn/ModernAndroid/part2/BookSearch/app/src/main/java/com/hoon/booksearch/ui/view/FavoriteFragment.kt
+++ inflearn/ModernAndroid/part2/BookSearch/app/src/main/java/com/hoon/booksearch/ui/view/FavoriteFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.ItemTouchHelper
@@ -14,6 +15,8 @@ import com.google.android.material.snackbar.Snackbar
 import com.hoon.booksearch.databinding.FragmentFavoriteBinding
 import com.hoon.booksearch.ui.adapter.BookSearchAdapter
 import com.hoon.booksearch.ui.viewmodel.BookSearchViewModel
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
 class FavoriteFragment : Fragment() {
     private var _binding: FragmentFavoriteBinding? = null
@@ -37,8 +40,15 @@ class FavoriteFragment : Fragment() {
 
         setupRecyclerView()
         setupTouchHelper(view)
-        bookSearchViewModel.favoriteBooks.observe(viewLifecycleOwner) {
-            bookSearchAdapter.submitList(it)
+
+//        bookSearchViewModel.favoriteBooks.observe(viewLifecycleOwner) {
+//            bookSearchAdapter.submitList(it)
+//        }
+
+        lifecycleScope.launch {
+            bookSearchViewModel.favoriteBooks.collectLatest {
+                bookSearchAdapter.submitList(it)
+            }
         }
     }
 
