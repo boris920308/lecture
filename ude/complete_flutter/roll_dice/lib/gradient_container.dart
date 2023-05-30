f26a86f758ed0234e646bbd878327242027a0863
boris920308 <jhoonj0308@gmail.com>
2023-05-30 15:50:42 +0900

    [ feat ][CompleteFlutter] #roll_dice 1.textbutton 2.onClick 3.Column

--- ude/complete_flutter/roll_dice/lib/gradient_container.dart
+++ ude/complete_flutter/roll_dice/lib/gradient_container.dart
@@ -1,14 +1,24 @@
 import 'package:flutter/material.dart';
-import 'package:roll_dice/styled_text.dart';
 
 const startAlignment = Alignment.topLeft;
 // Alignment? startAlignment;
 const endAlignment = Alignment.bottomRight;
 
 class GradientContainer extends StatelessWidget {
-  const GradientContainer(this.startColor, this.endColor, {super.key});
+  GradientContainer(this.startColor, this.endColor, {super.key});
+
+  GradientContainer.purple({super.key})
+      : startColor = Colors.deepPurple,
+        endColor = Colors.indigo;
+
   final Color startColor;
   final Color endColor;
+  var activeDiceImage = 'assets/images/dice-2.png';
+
+  void rollDice() {
+    activeDiceImage = 'assets/images/dice-4.png';
+    print('Changing image...');
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -25,8 +35,30 @@ class GradientContainer extends StatelessWidget {
           end: endAlignment,
         ),
       ),
-      child: const Center(
-        child: StyledText('Hello World!'),
+      child: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Image.asset(
+              'assets/images/dice-2.png',
+              width: 200,
+            ),
+            const SizedBox(height: 20),
+            TextButton(
+              onPressed: rollDice,
+              style: TextButton.styleFrom(
+                // padding: const EdgeInsets.only(
+                //   top: 20,
+                // ),
+                foregroundColor: Colors.white,
+                textStyle: const TextStyle(
+                  fontSize: 28,
+                ),
+              ),
+              child: const Text('Roll Dice'),
+            ),
+          ],
+        ),
       ),
     );
     // throw UnimplementedError();
--- ude/complete_flutter/roll_dice/lib/main.dart
+++ ude/complete_flutter/roll_dice/lib/main.dart
@@ -3,12 +3,12 @@ import 'package:roll_dice/gradient_container.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
       home: Scaffold(
         // backgroundColor: Color.fromARGB(255, 12, 58, 59),
         body: GradientContainer(
-          Color.fromARGB(255, 102, 107, 100),
-          Color.fromARGB(255, 204, 219, 221),
+          const Color.fromARGB(225, 206, 104, 104),
+          const Color.fromARGB(224, 160, 117, 117),
         ),
       ),
     ),
